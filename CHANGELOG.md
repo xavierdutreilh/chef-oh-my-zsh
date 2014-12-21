27b50ecd7c098804985d7305c845c57ca4eb29c5
Xavier Dutreilh <xavier@dutreilh.com>
2014-12-21 11:46:58 +0100

    Bump version to 0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,5 @@
 # chef-oh-my-zsh CHANGELOG
+
+## 0.1.0
+
+- [Xavier Dutreilh] - Initial release of chef-oh-my-zsh
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'xavier@dutreilh.com'
 license          'MIT'
 description      'Installs oh-my-zsh.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.0'
+version          '0.1.0'
 
 depends 'git'
 depends 'zsh'
